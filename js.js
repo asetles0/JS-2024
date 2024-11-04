51f0b952cab08ac921d514fb3d08e88dffb5418f
Aset <asetles0@gmail.com>
2024-11-04 19:12:00 +0500

    first commit

---
+++ js.js
@@ -0,0 +1,51 @@
+//var a = 5;
+//var b = 3;
+
+//console.log(a==b); //true
+
+//console.log(a!=b) //false
+
+//console.log(a = 3 || a > 5 || a = 7 || b = 3);
+
+//console.log(a = 4 || b = 4);
+// barluq shart
+//console.log(a = 5 && b = 3); // ! =
+
+//let isOffRoad = false;
+// ! emes
+//console.log(!isOffRoad); // true
+
+//var age = 18;
+//var hasTicket = true;
+//var isVIP = false;
+
+//if ((age 18 && hasTicket) || isVIP) { //>
+//    console.log("siz kire alasyz");
+//} else { // kez
+//    console.log("Bolmaidy")
+//}
+
+// var name = "BMW M3 F80";
+// var year = 2017;
+// var engine = 3.0;
+// var color = "white";
+
+// if (year  2014 && color ="white") {
+//     console.log("Alaminym");
+// } else if (engine = 3.0) {
+//     console.log("Meili, alamyn");
+// }
+
+//var a = 8;
+//var b = 8;
+//var p = (a + b) * 2;
+
+//if (a == b && p >= 40) (
+  //  else  (
+    //    console.log("false")
+    //)
+//)
+
+var age = 16;
+
+console.log("age");
